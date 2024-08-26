98caf64fca398c536e9601e88898f1f92c8bdd58
PricedCell <cameronbeattie97@gmail.com>
2024-08-26 21:46:07 +1000

    progress commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { connect } = require('http2');
+
 const inquirer = require('inquirer');
-const PG = require('pg');
+const { Pool } = require('pg');
 const { start } = require('repl');
 
-const pool = new pool ({
+const Pool = new Pool({
     host: "localhost",
     port: 5432,
     user: "root",
@@ -11,15 +11,22 @@ const pool = new pool ({
     database: "employee_tracker"
 });
 
-PG.connect();
+module.exports = pool;
 
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+// Add the case for "Update Role Salary" in the inquirer prompt response handler
 inquirer
 .prompt([
     {
-    type: "list",
-    message: "What would you like to do?",
-    name: "Menu",
-    choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"],
+        type: "list",
+        message: "What would you like to do?",
+        name: "Menu",
+        choices: ["View All Employees", "Add Employee", "Update Employee Role", "Delete Employee", "View All Roles", 
+            "Add Role", "View All Departments", "Add Department", "Update Employee Department", "Update Role Salary"],
     }
 ])
 .then((response) => {
@@ -28,16 +35,37 @@ inquirer
             viewEmployees();
             break;
         case "Add Employee":
-            addEmployee();
+            inquirer.prompt([
+                { type: 'input', name: 'name', message: 'Employee Name:' },
+                { type: 'input', name: 'role', message: 'Employee Role:' },
+                { type: 'input', name: 'department', message: 'Employee Department:' }
+            ]).then(addEmployee);
             break;
         case "Update Employee Role":
-            updateEmployee();
+            inquirer.prompt([
+                { type: 'input', name: 'id', message: 'Employee ID:' },
+                { type: 'input', name: 'newRole', message: 'New Role:' }
+            ]).then(({ id, newRole }) => updateEmployeeRole(id, newRole));
+            break;
+        case "Delete Employee":
+            inquirer.prompt([
+                { type: 'input', name: 'id', message: 'Employee ID:' }
+            ]).then(({ id }) => deleteEmployee(id));
             break;
         case "View All Roles":
             viewRoles();
             break;
         case "Add Role":
-            addRole();
+            inquirer.prompt([
+                { type: 'input', name: 'title', message: 'Role Title:' },
+                { type: 'input', name: 'salary', message: 'Role Salary:' },
+                { type: 'input', name: 'department_id', message: 'Department ID:' }
+            ]).then(addRole);
+            break;
+  
+            inquirer.prompt([
+                { type: 'input', name: 'id', message: 'Role ID:' }
+            ]).then(({ id }) => deleteRole(id));
             break;
         case "View All Departments":
             viewDepartments();
@@ -45,6 +73,19 @@ inquirer
         case "Add Department":
             addDepartment();
             break;
+        case "Update Employee Department":
+            inquirer.prompt([
+                { type: 'input', name: 'id', message: 'Employee ID:' },
+                { type: 'input', name: 'newDepartmentId', message: 'New Department ID:' }
+            ]).then(({ id, newDepartmentId }) => updateEmployeeDepartment(id, newDepartmentId));
+            break;
+        case "Update Role Salary":
+            inquirer.prompt([
+                { type: 'input', name: 'id', message: 'Role ID:' },
+                { type: 'input', name: 'salary', message: 'New Salary:' }
+            ]).then(({ id, salary }) => updateRoleSalary(id, salary));
+            break;
+        // Add other cases here
     }
 });
 
@@ -93,7 +134,7 @@ function addEmployee() {
     });
     
 }
-function updateEmployee() {
+function updateEmployeeRole() {
     connection.query ("SELECT * FROM employee", function(res) {
         inquirer
         .prompt([
@@ -131,14 +172,37 @@ function updateEmployee() {
     });
     console.log("Update Employee");
 }
+function deleteEmployee() {
+    connection.query("SELECT * FROM employee", function(res) {
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Select employee to delete",
+                name: "employee",
+                choices: res.map(employee => employee.First_Name)
+            }
+        ])
+        .then((response) => {
+            const employee = res.find(employee => employee.First_Name === response.employee);
+            connection.query("DELETE FROM employee WHERE id = ?", employee.id, function(err, res) {
+                if (err) throw err;
+                else {
+                    console.log('Employee deleted');
+                    start();
+                }
+            });
+        });
+    });
+    console.log("Delete Employee");
+}
 function viewRoles() {
-    connection.query("SELECT * FROM role", function(err, res) {
+    const query = "SELECT * FROM role";
+    connection.query(query, function(err, res) {
         if (err) throw err;
         console.table(res);
         start();
     });
-
-    console.log("View Roles");
 }
 function addRole() {
     connection.query("SELECT * FROM department", function(res) {
@@ -175,22 +239,20 @@ function addRole() {
                 }
             });
         });
-    
-
-    consolelog("Add Role"); });
+    });
+    console.log("Add Role");
 }
 function viewDepartments() {
-
-    connection.query("SELECT * FROM department", function(err, res) {
+    const query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
         if (err) throw err;
         console.table(res);
         start();
     });
-
-    console.log("View Departments");
 }
 function addDepartment() {
-    inquirer.prompt([
+    inquirer
+    .prompt([
         {
             type: "input",
             message: "Enter department name",
@@ -208,7 +270,78 @@ function addDepartment() {
             }
         });
     });
-
     console.log("Add Department");
+}
+function updateEmployeeDepartment() {
+    connection.query("SELECT * FROM employee", function(res) {
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Select employee to update",
+                name: "employee",
+                choices: res.map(employee => employee.First_Name)
+            }
+        ])
+        .then((response) => {
+            const employee = res.find(employee => employee.First_Name === response.employee);
+            connection.query("SELECT * FROM department", function(res) {
+                inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        message: "Select new department",
+                        name: "department",
+                        choices: res.map(department => department.name)
+                    }
+                ])
+                .then((response) => {
+                    const department = res.find(department => department.name === response.department);
+                    connection.query("UPDATE employee SET department_id = ? WHERE id = ?", [department.id, employee.id], function(err, res) {
+                        if (err) throw err;
+                        else {
+                            console.log('Employee updated');
+                            start();
+                        }
+                    });
+                });
+            });
+        });
+    });
+    console.log("Update Employee Department");
+}
+function updateRoleSalary() {
+    connection.query("SELECT * FROM role", function(res) {
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Select role to update",
+                name: "role",
+                choices: res.map(role => role.title)
+            }
+        ])
+        .then((response) => {
+            const role = res.find(role => role.title === response.role);
+            inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Enter new salary",
+                    name: "salary"
+                }
+            ])
+            .then((response) => {
+                connection.query("UPDATE role SET salary = ? WHERE id = ?", [response.salary, role.id], function(err, res) {
+                    if (err) throw err;
+                    else {
+                        console.log('Role updated');
+                        start();
+                    }
+                });
+            });
+        });
+    });
+    console.log("Update Role Salary");
 }
 
