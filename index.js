268ee1bd5b276b69f979efad64594bff823a1f0e
PricedCell <cameronbeattie97@gmail.com>
2024-08-26 18:57:40 +1000

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const { connect } = require('http2');
 const inquirer = require('inquirer');
-const {pool} = require('pg');
+const PG = require('pg');
 const { start } = require('repl');
 
-const pool = new Pool ({
+const pool = new pool ({
     host: "localhost",
     port: 5432,
     user: "root",
@@ -10,7 +11,7 @@ const pool = new Pool ({
     database: "employee_tracker"
 });
 
-pool.connect();
+PG.connect();
 
 inquirer
 .prompt([
@@ -93,18 +94,121 @@ function addEmployee() {
     
 }
 function updateEmployee() {
+    connection.query ("SELECT * FROM employee", function(res) {
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Select employee to update",
+                name: "employee",
+                choices: res.map(employee => employee.First_Name)
+            }
+        ])
+        .then((response) => {
+            const employee = res.find(employee => employee.First_Name === response.employee);
+            connection.query("SELECT * FROM role", function(res) {
+                inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        message: "Select new role",
+                        name: "role",
+                        choices: res.map(role => role.title)
+                    }
+                ])
+                .then((response) => {
+                    const role = res.find(role => role.title === response.role);
+                    connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [role.id, employee.id], function(err, res) {
+                        if (err) throw err;
+                        else () => {
+                            console.log('Employee updated');
+                            start();
+                        }
+                    });
+                });
+            });
+        });
+    });
     console.log("Update Employee");
 }
 function viewRoles() {
+    connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+
     console.log("View Roles");
 }
 function addRole() {
-    console.log("Add Role");
+    connection.query("SELECT * FROM department", function(res) {
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter role title",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Enter role salary",
+                name: "salary"
+            },
+            {
+                type: "list",
+                message: "Select role department",
+                name: "department",
+                choices: res.map(department => department.name)
+            }
+        ])
+        .then((response) => {
+            const department = res.find(department => department.name === response.department);
+            connection.query("INSERT INTO role SET ?", {
+                title: response.title,
+                salary: response.salary,
+                department_id: department.id
+            }, function(err, res) {
+                if (err) throw err;
+                else {
+                    console.log('Role added');
+                    start();
+                }
+            });
+        });
+    
+
+    consolelog("Add Role"); });
 }
 function viewDepartments() {
+
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+
     console.log("View Departments");
 }
 function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter department name",
+            name: "name"
+        }
+    ])
+    .then((response) => {
+        connection.query("INSERT INTO department SET ?", {
+            name: response.name
+        }, function(err, res) {
+            if (err) throw err;
+            else {
+                console.log('Department added');
+                start();
+            }
+        });
+    });
+
     console.log("Add Department");
 }
 
